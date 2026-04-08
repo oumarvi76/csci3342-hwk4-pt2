f251f40bd406095c3a94b5e326b0ff723c5956cd
oumarvi76 <omar_vi122076@outlook.com>
2026-04-08 17:51:49 -0500

    Delete hwk4/aurumGold directory

--- hwk4/aurumGold/autumGoldApp/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-from django.template import loader
-
-def home(request):
-    context = {}
-    template = loader.get_template("index.html")
-    return HttpResponse(template.render(context, request)) # from this template project,
